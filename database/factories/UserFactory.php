974ad09b12f1e2b8466b7a2b2be9161e3d5d9450
Tjoosten <Tjoosten3@gmail.com>
2017-11-01 13:58:21 +0100

    Add first and last name support

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,9 +17,11 @@ $factory->define(App\User::class, function (Faker $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'firstName'      => $faker->firstName,
+        'lastName'       => $faker->lastName,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
